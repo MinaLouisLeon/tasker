1979c299b463819284228bf34349c6fd2746e950
MinaLouisLeon <31774352+MinaLouisLeon@users.noreply.github.com>
2021-12-30 04:29:09 -0800

    Update firebaseConfig.js

--- src/api/firebaseConfig.js
+++ src/api/firebaseConfig.js
@@ -9,13 +9,7 @@ import { getFirestore } from "firebase/firestore"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   //firebaseConfig
-  apiKey: "AIzaSyCdN9REVvhFi9uGLmqQ-H8own7RSf6t6HU",
-  authDomain: "tasker-729e5.firebaseapp.com",
-  projectId: "tasker-729e5",
-  storageBucket: "tasker-729e5.appspot.com",
-  messagingSenderId: "1052534094905",
-  appId: "1:1052534094905:web:9a683d74e394e7c04561e8",
-  measurementId: "G-GV8SGCMY9P"
+ 
 };
 
 // Initialize Firebase
