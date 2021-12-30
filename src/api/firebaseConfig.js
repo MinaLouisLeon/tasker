9c3c340742fef9bf0499ed3f3a70accceadc3bf4
MinaLouisLeon <31774352+MinaLouisLeon@users.noreply.github.com>
2021-12-30 04:04:14 -0800

    ..

--- src/api/firebaseConfig.js
+++ src/api/firebaseConfig.js
@@ -9,6 +9,7 @@ import { getFirestore } from "firebase/firestore"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   //firebaseConfig
+ 
 };
 
 // Initialize Firebase
